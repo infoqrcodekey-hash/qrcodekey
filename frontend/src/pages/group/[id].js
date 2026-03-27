d2c4dbc1f6dd0d6456b2338d5526369a22ec6aa0
infoqrcodekey-hash <info.qrcodekey@gmail.com>
2026-03-27 13:06:11 -0500

    Fix: JSX syntax error in [id].js dashboard - extra closing paren

--- frontend/src/pages/group/[id].js
+++ frontend/src/pages/group/[id].js
@@ -298,7 +298,7 @@ export default function GroupDashboard() {
                     </button>
                   </div>
                 </div>
-              )))
+              ))
             )}
           </div>
         )}
